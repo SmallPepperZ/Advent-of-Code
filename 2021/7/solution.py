33469db2befb06baa03f556b7b84404d16eb2ed0
SmallPepperZ <69822582+SmallPepperZ@users.noreply.github.com>
2022-01-05 18:54:31 -0800

    Day 7

---
+++ 2021/7/solution.py
@@ -0,0 +1,34 @@
+## advent of code 2021
+## https://adventofcode.com/2021
+## day 7
+
+from statistics import median
+
+def cost(distance:int) -> int:
+    return (distance**2+distance)/2
+
+
+
+def avg(data:list[int]):
+    return sum(data)/len(data)
+
+def parse_input(lines):
+    return [int(val) for val in lines[0].split(",")]
+
+def part1(data):
+    target = int(median(data))
+    fuel = 0
+    for val in data:
+        fuel += abs(val-target)
+    return int(fuel)
+
+
+def part2(data):
+    targetA = int(avg(data))
+    targetB = int(avg(data))+1
+    fuelA = 0
+    fuelB = 0
+    for val in data:
+        fuelA += cost(abs(val-targetA))
+        fuelB += cost(abs(val-targetB))
+    return int(min(fuelA, fuelB))
